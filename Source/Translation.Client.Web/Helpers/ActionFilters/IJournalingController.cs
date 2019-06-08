a531ca12b2ca43fd32ab34b23c0d89df35d5de3e
serdarb <hserdarb@gmail.com>
2019-06-08 16:22:04 +0300

    Add journal filter

---
+++ Source/Translation.Client.Web/Helpers/ActionFilters/IJournalingController.cs
@@ -0,0 +1,11 @@
+﻿using Translation.Common.Contracts;
+using Translation.Common.Models.Shared;
+
+namespace Translation.Client.Web.Helpers.ActionFilters
+{
+    public interface IJournalingController
+    {
+        IJournalService JournalService { get; set; }
+        CurrentUser CurrentUser { get; }
+    }
+}
---
+++ Source/Translation.Client.Web/Helpers/ActionFilters/JournalFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+using Translation.Common.Exceptions;
+using Translation.Common.Helpers;
+using Translation.Common.Models.Requests.Journal;
+
+namespace Translation.Client.Web.Helpers.ActionFilters
+{
+    public class JournalFilter : ActionFilterAttribute
+    {
+        public string Message { get; set; }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var controller = context.Controller as IJournalingController;
+            if (controller == null)
+            {
+                return;
+            }
+
+            if (Message.IsEmpty())
+            {
+                return;
+            }
+
+            if (!controller.CurrentUser.IsActionSucceed)
+            {
+                return;
+            }
+
+            var request = new JournalCreateRequest(controller.CurrentUser.Id, Message);
+            var response = controller.JournalService.CreateJournal(request);
+            if (response.Status.IsNotSuccess)
+            {
+                throw new JournalException("couldn't write to journal > " + Message);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
