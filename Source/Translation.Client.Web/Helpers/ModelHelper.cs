8d2783f2795548ac2911535f840b56033177ba72
serdarb <hserdarb@gmail.com>
2019-06-08 16:16:51 +0300

    Create ModelHelper.cs

---
+++ Source/Translation.Client.Web/Helpers/ModelHelper.cs
@@ -0,0 +1,19 @@
+﻿using Translation.Client.Web.Models;
+using Translation.Client.Web.Models.Base;
+using Translation.Common.Models.Base;
+
+namespace Translation.Client.Web.Helpers
+{
+    public static class ModelHelper
+    {
+        public static void MapMessages(this BaseModel model, BaseResponse response)
+        {
+            model.ErrorMessages.AddRange(response.ErrorMessages);
+            model.InfoMessages.AddRange(response.InfoMessages);
+            model.WarningMessages.AddRange(response.WarningMessages);
+            model.SuccessMessages.AddRange(response.SuccessMessages);
+
+            model.SetInputModelValues();
+        }
+    }
+}
