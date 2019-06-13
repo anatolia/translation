a353311d3b5eed40fabecdc140aab82cd427c0ae
serdarb <hserdarb@gmail.com>
2019-06-13 13:55:09 +0300

    Add test project

--- Test/Translation.Tests/DomainTests/ProjectDomainTests.cs
+++ Test/Translation.Tests/DomainTests/ProjectDomainTests.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using NUnit.Framework;
 
-using NUnit.Framework;
-
-using Translation.Data.Entities.Main;
-using Translation.Data.Entities.Project;
-using Translation.Data.Entities.Parameter;
+using Translation.Data.Entities.Domain;
 
 namespace Translation.Tests.DomainTests
 {
@@ -12,66 +8,10 @@ namespace Translation.Tests.DomainTests
     public class ProjectDomainTests
     {
         [Test]
-        public void Organization_Has_Projects()
-        {
-            var organization = new Organization();
-            Assert.IsAssignableFrom<List<Project>>(organization.Projects);
-        }
-
-        [Test]
         public void Project_Keeps_LabelCount()
         {
             var project = new Project();
             Assert.IsAssignableFrom<int>(project.LabelCount);
-        }
-
-        [Test]
-        public void Label_Has_Translations()
-        {
-            var label = new Label();
-            Assert.IsAssignableFrom<List<LabelTranslation>>(label.Translations);
-        }
-
-        [Test]
-        public void Translation_Has_Language()
-        {
-            var languageProperty = typeof(LabelTranslation).GetProperty(nameof(Language));
-            Assert.True(languageProperty.PropertyType.Name == typeof(Language).Name);
-        }
-
-        [Test]
-        public void Organization_Has_Integrations()
-        {
-            var organization = new Organization();
-            Assert.IsAssignableFrom<List<Integration>>(organization.Integrations);
-        }
-
-        [Test]
-        public void Integration_Has_Roles()
-        {
-            var integration = new Integration();
-            Assert.IsAssignableFrom<List<Role>>(integration.Roles);
-        }
-
-        [Test]
-        public void Organization_Has_Users()
-        {
-            var organization = new Organization();
-            Assert.IsAssignableFrom<List<User>>(organization.Users);
-        }
-
-        [Test]
-        public void User_Has_Roles()
-        {
-            var user = new User();
-            Assert.IsAssignableFrom<List<Role>>(user.Roles);
-        }
-
-        [Test]
-        public void Role_Has_Permissions()
-        {
-            var role = new Role();
-            Assert.IsAssignableFrom<List<Permission>>(role.Permissions);
         }
     }
 }
--- Test/Translation.Tests/DomainTests/SchemaTests.cs
+++ Test/Translation.Tests/DomainTests/SchemaTests.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 
 using NUnit.Framework;
+using StandardRepository.Models.Entities.Schemas;
 
-using Translation.Data.Entities.Base.Schemas;
+using Translation.Data.Entities.Domain;
 using Translation.Data.Entities.Main;
 using Translation.Data.Entities.Parameter;
-using Translation.Data.Entities.Project;
 
 namespace Translation.Tests.DomainTests
 {
     [TestFixture]
     public class SchemaTests
     {
-        [TestCase(typeof(Organization))]
-        [TestCase(typeof(Project))]
-        [TestCase(typeof(Label))]
-        [TestCase(typeof(LabelTranslation))]
+        [TestCase(typeof(Project)), 
+         TestCase(typeof(Label)), 
+         TestCase(typeof(LabelTranslation))]
         public void ProjectEntityClasses_Has_ISchemaProject(Type entity)
         {
-            var instance = (ISchemaProject) Activator.CreateInstance(entity);
+            var instance = (ISchemaDomain)Activator.CreateInstance(entity);
             Assert.IsNotNull(instance);
         }
 
-        [TestCase(typeof(Language))]
-        [TestCase(typeof(Word))]
-        [TestCase(typeof(WordTranslation))]
+        [TestCase(typeof(Language)),
+         TestCase(typeof(Word)),
+         TestCase(typeof(WordTranslation))]
         public void ParameterEntityClasses_Has_ISchemaParameter(Type entity)
         {
             var instance = (ISchemaParameter)Activator.CreateInstance(entity);
             Assert.IsNotNull(instance);
         }
 
-        [TestCase(typeof(Integration))]
-        [TestCase(typeof(Token))]
-        [TestCase(typeof(TokenRequestLog))]
-        [TestCase(typeof(User))]
-        [TestCase(typeof(UserLoginLog))]
-        [TestCase(typeof(Role))]
-        [TestCase(typeof(Permission))]
-        [TestCase(typeof(PermissionLog))]
-        [TestCase(typeof(Journal))]
+        [TestCase(typeof(Organization)), 
+         TestCase(typeof(Integration)),
+         TestCase(typeof(IntegrationClient)),
+         TestCase(typeof(Token)),
+         TestCase(typeof(TokenRequestLog)),
+         TestCase(typeof(User)),
+         TestCase(typeof(UserLoginLog)),
+         TestCase(typeof(SendEmailLog)),
+         TestCase(typeof(Journal))]
         public void MainEntityClasses_Has_ISchemaMain(Type entity)
         {
             var instance = (ISchemaMain)Activator.CreateInstance(entity);
