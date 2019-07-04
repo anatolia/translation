df6f4b13f7289a31959cf389ee9f31d470b0124d
serdarsen <serdarsendev@gmail.com>
2019-07-04 11:22:06 +0300

    Update FakeModelTestHelper.cs

--- Test/Translation.Tests/TestHelpers/FakeModelTestHelper.cs
+++ Test/Translation.Tests/TestHelpers/FakeModelTestHelper.cs
@@ -1,7 +1,48 @@
-﻿namespace Translation.Tests.TestHelpers
+﻿using Translation.Common.Models.Shared;
+using static Translation.Tests.TestHelpers.FakeConstantTestHelper;
+
+namespace Translation.Tests.TestHelpers
 {
     public class FakeModelTestHelper
     {
-        
+        public static PagingInfo GetFakePagingInfo()
+        {
+            var pagingInfo = new PagingInfo();
+            pagingInfo.Skip = One;
+            pagingInfo.Take = Two;
+            pagingInfo.LastUid = UidOne;
+            pagingInfo.IsAscending = BooleanTrue;
+            pagingInfo.TotalItemCount = Ten;
+            pagingInfo.Type = PagingInfo.PAGE_NUMBERS;
+
+            return pagingInfo;
+        }
+
+
+        public static PagingInfo GetFakePagingInfoForSelectMany()
+        {
+            var pagingInfo = new PagingInfo();
+            pagingInfo.Skip = One;
+            pagingInfo.Take = Two;
+            pagingInfo.LastUid = UidOne;
+            pagingInfo.IsAscending = BooleanTrue;
+            pagingInfo.TotalItemCount = Ten;
+            pagingInfo.Type = PagingInfo.PAGE_NUMBERS;
+
+            return pagingInfo;
+        }
+
+        public static PagingInfo GetFakePagingInfoForSelectAfter()
+        {
+            var pagingInfo = new PagingInfo();
+            pagingInfo.Skip = Zero;
+            pagingInfo.Take = Two;
+            pagingInfo.LastUid = UidOne;
+            pagingInfo.IsAscending = BooleanTrue;
+            pagingInfo.TotalItemCount = Ten;
+            pagingInfo.Type = PagingInfo.PAGE_NUMBERS;
+
+            return pagingInfo;
+        }
     }
 }
