b9b00b5d5592c0fc9fd65e963b88df0826e6cc69
humite <humite@gmail.com>
2019-07-04 18:07:22 +0300

    entrypoint script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Changing Translation.Client.Web.dll.config file"
+sed -i -e "s/_DBUSER_/"$DBUSER"/" \
+       -e "s/_DBPASS_/"$DBPASS"/" \
+       -e "s/_DBCONN_/"$DBCONN"/" \
+       -e "s/_DBNAME_/"$DBNAME"/" \
+       -e "s/_SUPERP_/"$SUPERP"/" Translation.Client.Web.dll.config
+
+echo "Starting dotnet Translation.Client.Web.dll"
+dotnet Translation.Client.Web.dll
