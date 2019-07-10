344a87bf376a04c2d90fda0d10bd1e8c81f93035
serdarsen <serdarsendev@gmail.com>
2019-07-10 17:49:38 +0300

    some tests refactoring

--- Test/Translation.Tests/Client/Controllers/ProjectControllerTests.cs
+++ Test/Translation.Tests/Client/Controllers/ProjectControllerTests.cs
@@ -1,13 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+using Translation.Client.Web.Controllers;
+using Translation.Client.Web.Models.Project;
+using Translation.Tests.TestHelpers;
+using static Translation.Tests.TestHelpers.ActionMethodNameConstantTestHelper;
 
 namespace Translation.Tests.Client.Controllers
 {
     [TestFixture]
-    public class ProjectControllerTests
+    public class ProjectControllerTests : ControllerBaseTests
     {
+        public ProjectController SystemUnderTest { get; set; }
+
         [SetUp]
         public void run_before_every_test()
         {
+            SystemUnderTest = Container.Resolve<ProjectController>();
+            SetControllerContext(SystemUnderTest);
+        }
+
+        [TestCase(Create, new[] { typeof(Guid) }, typeof(HttpGetAttribute))]
+        [TestCase(Create, new[] { typeof(ProjectCreateModel) }, typeof(HttpPostAttribute))]
+        [TestCase(Detail, new[] { typeof(Guid) }, typeof(HttpGetAttribute))]
+        [TestCase(Edit, new[] { typeof(Guid) }, typeof(HttpGetAttribute))]
+        [TestCase(Edit, new[] { typeof(ProjectEditModel) }, typeof(HttpPostAttribute))]
+        [TestCase(Delete, new[] { typeof(Guid) }, typeof(HttpPostAttribute))]
+        [TestCase(Clone, new[] { typeof(Guid) }, typeof(HttpGetAttribute))]
+        [TestCase(Clone, new[] { typeof(ProjectCloneModel) }, typeof(HttpPostAttribute))]
+        [TestCase(SelectData, new Type[] { }, typeof(HttpGetAttribute))]
+        [TestCase(PendingTranslations, new[] { typeof(Guid) }, typeof(HttpGetAttribute))]
+        [TestCase(PendingTranslationsData, new[] { typeof(Guid), typeof(int), typeof(int) }, typeof(HttpGetAttribute))]
+        [TestCase(LabelListData, new[] { typeof(Guid), typeof(int), typeof(int) }, typeof(HttpGetAttribute))]
+        [TestCase(DownloadLabels, new[] { typeof(Guid) }, typeof(HttpPostAttribute))]
+        [TestCase(ChangeActivation, new[] { typeof(Guid), typeof(Guid) }, typeof(HttpPostAttribute))]
+        [TestCase(Revisions, new[] { typeof(Guid)}, typeof(HttpGetAttribute))]
+        [TestCase(RevisionsData, new[] { typeof(Guid)}, typeof(HttpGetAttribute))]
+        [TestCase(Restore, new[] { typeof(Guid), typeof(int)}, typeof(HttpPostAttribute))]
+        public void Methods_Has_Http_Verb_Attributes(string actionMethod, Type[] parameters, Type httpVerbAttribute)
+        {
+            var type = SystemUnderTest.GetType();
+            var methodInfo = type.GetMethod(actionMethod, parameters);
+            var attributes = methodInfo.GetCustomAttributes(httpVerbAttribute, true);
+            Assert.AreEqual(attributes.Length, 1);
         }
     }
 }
---
+++ Test/Translation.Tests/TestHelpers/ActionMethodNameConstantTestHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace Translation.Tests.TestHelpers
+{
+    public class ActionMethodNameConstantTestHelper
+    {
+        public const string Create = "Create";
+        public const string Edit = "Edit";
+        public const string Detail = "Detail";
+        public const string Clone = "Clone";
+        public const string Delete = "Delete";
+        public const string ListData = "ListData";
+        public const string SelectData = "SelectData";
+        public const string ChangeActivation = "ChangeActivation";
+
+        public const string Revisions = "Revisions";
+        public const string RevisionsData = "RevisionsData";
+        public const string Restore = "Restore";
+
+        public const string PendingTranslations = "PendingTranslations";
+        public const string PendingTranslationsData = "PendingTranslationsData";
+
+        public const string LabelListData= "LabelListData";
+        public const string DownloadLabels = "DownloadLabels";
+    }
+}
