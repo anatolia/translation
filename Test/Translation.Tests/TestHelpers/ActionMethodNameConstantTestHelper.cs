9a77c0a183aa118503a64e63a047c159c98ff2d5
serdarsen <serdarsendev@gmail.com>
2019-07-12 17:55:51 +0300

    some constant names changed

--- Test/Translation.Tests/TestHelpers/ActionMethodNameConstantTestHelper.cs
+++ Test/Translation.Tests/TestHelpers/ActionMethodNameConstantTestHelper.cs
@@ -2,23 +2,54 @@
 {
     public class ActionMethodNameConstantTestHelper
     {
-        public const string Create = "Create";
-        public const string Edit = "Edit";
-        public const string Detail = "Detail";
-        public const string Clone = "Clone";
-        public const string Delete = "Delete";
-        public const string ListData = "ListData";
-        public const string SelectData = "SelectData";
-        public const string ChangeActivation = "ChangeActivation";
+        public const string CreateAction = "Create";
+        public const string EditAction = "Edit";
+        public const string DetailAction = "Detail";
+        public const string CloneAction = "Clone";
+        public const string DeleteAction = "Delete";
+        public const string ListAction = "List";
+        public const string ListDataAction = "ListData";
+        public const string SelectDataAction = "SelectData";
+        public const string ChangeActivationAction = "ChangeActivation";
 
-        public const string Revisions = "Revisions";
-        public const string RevisionsData = "RevisionsData";
-        public const string Restore = "Restore";
+        public const string InviteAction = "Invite";
+        public const string InviteDoneAction = "InviteDone";
 
-        public const string PendingTranslations = "PendingTranslations";
-        public const string PendingTranslationsData = "PendingTranslationsData";
+        public const string OrganizationListAction = "OrganizationList";
+        public const string OrganizationListDataAction = "OrganizationListData";
 
-        public const string LabelListData= "LabelListData";
-        public const string DownloadLabels = "DownloadLabels";
+        public const string UserListAction = "UserList";
+        public const string UserListDataAction = "UserListData";
+
+        public const string UserLoginLogListAction = "UserLoginLogList";
+        public const string UserLoginLogListDataAction = "UserLoginLogListData";
+
+        public const string JournalListAction = "JournalList";
+        public const string JournalListDataAction = "JournalListData";
+
+        public const string TokenRequestLogListAction = "TokenRequestLogList";
+        public const string TokenRequestLogListDataAction = "TokenRequestLogListData";
+
+        public const string SendEmailLogListAction = "SendEmailLogList";
+        public const string SendEmailLogListDataAction = "SendEmailLogListData";
+
+        public const string RevisionsAction = "Revisions";
+        public const string RevisionsDataAction = "RevisionsData";
+        public const string RestoreAction = "Restore";
+
+        public const string PendingTranslationsAction = "PendingTranslations";
+        public const string PendingTranslationsDataAction = "PendingTranslationsData";
+
+        public const string LabelListDataAction = "LabelListData";
+        public const string DownloadLabelsAction = "DownloadLabels";
+
+        public const string OrganizationChangeActivationAction = "OrganizationChangeActivation";
+
+        public const string DashboardAction = "Dashboard";
+        public const string DegradeToUserAction = "DegradeToUser";
+        public const string UserUpgradeToAdminAction = "UserUpgradeToAdmin";
+
+        public const string AcceptInviteAction = "AcceptInvite";
+        public const string AcceptInviteDoneAction = "AcceptInviteDone";
     }
 }
