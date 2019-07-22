5eeee0cdc2c332c64825316e0f2a322d5a0f0601
Serdar Büyüktemiz <hserdarb@gmail.com>
2019-07-22 16:40:00 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ sed -i -e "s/_DBUSER_/"$DBUSER"/" \
        -e "s/_DBPASS_/"$DBPASS"/" \
        -e "s/_DBCONN_/"$DBCONN"/" \
        -e "s/_DBNAME_/"$DBNAME"/" \
+       -e "s/_DBPORT_/"$DBPORT"/" \
        -e "s/_SUPERP_/"$SUPERP"/" Translation.Client.Web.dll.config
 
 echo "Starting dotnet Translation.Client.Web.dll"
