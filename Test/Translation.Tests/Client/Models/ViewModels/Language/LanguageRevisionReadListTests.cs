f35a43efec30b97b901303228186d0d0e38a5f33
CihatAskin <moralman26@gmail.com>
2019-07-29 09:12:42 +0300

    Some model tests of Language added, testhelper and csproj refactoring

---
+++ Test/Translation.Tests/Client/Models/ViewModels/Language/LanguageRevisionReadListTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+using Translation.Client.Web.Models.Language;
+using static Translation.Tests.TestHelpers.FakeModelTestHelper;
+
+
+namespace Translation.Tests.Client.Models.ViewModels.Language
+{
+    [TestFixture]
+    public class LanguageRevisionReadListTests
+    {
+        public LanguageRevisionReadListModel SystemUnderTest { get; set; }
+
+        [SetUp]
+        public void run_before_every_test()
+        {
+            SystemUnderTest = GetLanguageRevisionReadListModel();
+        }
+
+        [Test]
+        public void LanguageRevisionReadListModel_Title()
+        {
+            Assert.AreEqual(SystemUnderTest.Title, "language_revision_list_title");
+        }
+
+    }
+}
