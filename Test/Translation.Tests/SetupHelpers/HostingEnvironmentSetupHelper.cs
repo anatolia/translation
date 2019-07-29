3ffd21733e1475c5b7b3aa8a2e1ffb506df391b0
serdarsen <serdarsendev@gmail.com>
2019-07-29 10:20:09 +0300

    HostingEnvironmentSetupHelper added

---
+++ Test/Translation.Tests/SetupHelpers/HostingEnvironmentSetupHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+using Moq;
+
+using static Translation.Tests.TestHelpers.FakeEntityTestHelper;
+
+namespace Translation.Tests.SetupHelpers
+{
+    public static class HostingEnvironmentSetupHelper
+    {
+        public static void Setup_WebRootPath_Returns_WebRootPath(this Mock<IHostingEnvironment> environment)
+        {
+            environment.Setup(x => x.WebRootPath).Returns(GetWebRootPath());
+        }
+
+        public static void Verify_WebRootPath(this Mock<IHostingEnvironment> environment)
+        {
+            environment.Verify(x => x.WebRootPath);
+        }
+    }
+}
