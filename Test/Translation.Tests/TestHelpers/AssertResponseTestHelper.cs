9e26abcb2a7a86eb1f4a37d131dcfc3f0da072dd
serdarsen <serdarsendev@gmail.com>
2019-07-31 17:54:59 +0300

    removed underline from two_word entity name messages

--- Test/Translation.Tests/TestHelpers/AssertResponseTestHelper.cs
+++ Test/Translation.Tests/TestHelpers/AssertResponseTestHelper.cs
@@ -20,20 +20,20 @@ namespace Translation.Tests.TestHelpers
         public const string UserNotFound = "user_not_found";
         public const string LanguageNotFound = "language_not_found";
         public const string IntegrationNotFound = "integration_not_found";
-        public const string IntegrationClientNotFound = "integration_client_not_found";
+        public const string IntegrationClientNotFound = "integrationclient_not_found";
         public const string TokenNotFound = "token_not_found";
         public const string LabelNotFound = "label_not_found";
-        public const string LabelTranslationNotFound = "label_translation_not_found";
+        public const string LabelTranslationNotFound = "labeltranslation_not_found";
 
         public const string OrganizationNotActive = "organization_not_active";
         public const string ProjectNotActive = "project_not_active";
         public const string UserNotActive = "user_not_active";
         public const string LanguageNotActive = "language_not_active";
         public const string IntegrationNotActive = "integration_not_active";
-        public const string IntegrationClientNotActive = "integration_client_not_active";
+        public const string IntegrationClientNotActive = "integrationclient_not_active";
         public const string TokenNotActive = "token_not_active";
         public const string LabelNotActive = "label_not_active";
-        public const string LabelTranslationNotActive = "label_translation_not_active";
+        public const string LabelTranslationNotActive = "labeltranslation_not_active";
 
         public const string OrganizationRevisionNotFound = "organization_revision_not_found";
         public const string ProjectRevisionNotFound = "project_revision_not_found";
@@ -43,17 +43,17 @@ namespace Translation.Tests.TestHelpers
         public const string IntegrationRevisionClientNotFound = "integration_revision_client_not_found";
         public const string TokenRevisionNotFound = "token_revision_not_found";
         public const string LabelRevisionNotFound = "label_revision_not_found";
-        public const string LabelTranslationRevisionNotFound = "label_translation_revision_not_found";
+        public const string LabelTranslationRevisionNotFound = "labeltranslation_revision_not_found";
 
         public const string OrganizationHasChildren = "organization_has_children";
         public const string ProjectHasChildren = "project_has_children";
         public const string UserHasChildren = "user_has_children";
         public const string LanguageHasChildren = "language_has_children";
         public const string IntegrationHasChildren = "integration_has_children";
-        public const string IntegrationClientHasChildren = "integration_client_has_children";
+        public const string IntegrationClientHasChildren = "integrationclient_has_children";
         public const string TokenHasChildren = "token_has_children";
         public const string LabelHasChildren = "label_has_children";
-        public const string LabelTranslationHasChildren = "label_translation_has_children";
+        public const string LabelTranslationHasChildren = "labeltranslation_has_children";
 
         public const string UserNotSuperAdmin = "user_not_super_admin";
         public const string UserNotAdmin = "user_not_admin";
