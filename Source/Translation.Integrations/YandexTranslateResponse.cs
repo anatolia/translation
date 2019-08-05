b008d9462e4b402d02a424ae9f4ed876fade02a7
serdarsen <serdarsendev@gmail.com>
2019-08-05 11:41:16 +0300

    Realtime cloud translate system refactoring

---
+++ Source/Translation.Integrations/YandexTranslateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Translation.Integrations
+{
+    public class YandexTranslateResponse
+    {
+        public int Code { get; set; }
+        public string Lang { get; set; }
+        public List<string> Text { get; set; }
+    }
+}
