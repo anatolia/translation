22b8d30bde4e60df2462430b63d5e67b1de00f77
serdarsen <serdarsendev@gmail.com>
2019-08-05 17:55:31 +0300

    some user model tests added, FakeModelTestHelper refactoring

---
+++ Test/Translation.Tests/Client/Models/ViewModels/User/AllUserListModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using Translation.Client.Web.Models.User;
+using Translation.Tests.TestHelpers;
+
+namespace Translation.Tests.Client.Models.ViewModels.User
+{
+    [TestFixture]
+    public sealed class AllUserListModelTests
+    {
+        public AllUserListModel SystemUnderTest { get; set; }
+
+        [SetUp]
+        public void run_before_every_test()
+        {
+            SystemUnderTest = FakeModelTestHelper.GetAllUserListModel();
+        }
+
+        [Test]
+        public void AllUserListModel_Title()
+        {
+            Assert.AreEqual(SystemUnderTest.Title, "user_list_title");
+        }
+    }
+}
