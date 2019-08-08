08a983f12f24439f1fe8c3cf41ac4cbb2956eebd
HASANCABUK <hasan_cabuk_@hotmail.com>
2019-08-08 17:56:48 +0300

    Update Site js

--- Source/Translation.Client.Web/wwwroot/js/site.js
+++ Source/Translation.Client.Web/wwwroot/js/site.js
@@ -98,32 +98,33 @@ function doRedirectIfConfirmedSuccess(btn, redirectUrl) {
     });
 }
 
-let labels = JSON.parse(localStorage.getItem('translations'));
-
 function translateScreen() {
     translateElement(document.head);
     translateElement(document.body);
 }
 
 function translateElement(element) {
-    if (element === null || element === undefined) {
+    if (element === null
+        || element === undefined) {
         return;
     }
-    
-    if (labels === null || labels === undefined) {
+
+    if (labels === null
+        || labels === undefined) {
         return;
     }
-    
+
     let defaultLang = 'en';
-    if (currentUser !== undefined && currentUser !== null) {
+    if (currentUser !== undefined
+        && currentUser !== null) {
         defaultLang = currentUser.language;
     }
-    
+
     let placeholders = element.querySelectorAll('[placeholder]');
     placeholders.forEach(function (item) {
         for (let i = 0; i < labels.length; i++) {
             let label = labels[i];
-            
+
             if (label.key === item.placeholder) {
                 label.translations.forEach(function (translation) {
                     if (translation.languageIsoCode2 === defaultLang) {
@@ -131,12 +132,12 @@ function translateElement(element) {
                         return;
                     }
                 });
-                
+
                 break;
             }
         }
     });
-    
+
     let items = element.querySelectorAll('[data-translation]');
     items.forEach(function (item) {
         for (let i = 0; i < labels.length; i++) {
@@ -148,35 +149,35 @@ function translateElement(element) {
                         return;
                     }
                 });
-                
+
                 break;
             }
         }
     });
 }
 
+let currentUser = null;
 doGet('/Data/GetCurrentUser', function (req) {
 
-    console.log("req.responseText: ", req.responseText);
     if (199 < req.status && req.status < 300) {
         if (req.status === 200
             && req.responseText !== null) {
             localStorage.setItem('currentUser', req.responseText);
+            currentUser = JSON.parse(localStorage.getItem('currentUser'));
         } else if (req.status === 200
             && req.responseText === null) {
+            currentUser = null;
             window.redirect('/Login');
         }
     }
 });
 
-if (!labels) {
+let labels = JSON.parse(localStorage.getItem('labels'));
+if (labels === null) {
     doGet('/Data/GetMainLabels', function (req) {
-        if (199 < req.status && req.status < 300) {
-            if (result.isOk) {
-                labels = result.item;
-                localStorage.setItem('labels', req.responseText);
-                translateScreen();
-            }    
+        if (199 < req.status && req.status < 300) {        
+            labels = localStorage.setItem('labels', req.responseText);         
+            translateScreen();
         }
     });
 } else {
