342dadabeb5c925482e49d0ede0a3ec62d78065c
CihatAskin <moralman26@gmail.com>
2019-08-15 18:34:53 +0300

    refactoring TranslationProvider classes using with json file directly when translation provider was edited dashboard

--- Source/Translation.Integrations/Providers/GoogleTranslateProvider.cs
+++ Source/Translation.Integrations/Providers/GoogleTranslateProvider.cs
@@ -1,34 +1,50 @@
 ﻿using System.Configuration;
 using System.IO;
 using System.Threading.Tasks;
-
+using System.Threading.Tasks.Dataflow;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Translation.V2;
-
+using Newtonsoft.Json.Linq;
+using StandardRepository.Helpers;
 using Translation.Common.Contracts;
+using Translation.Data.Repositories.Contracts;
 
 namespace Translation.Integrations.Providers
 {
     public class GoogleTranslateProvider : ITextTranslateProvider
     {
-        public string GoogleApplicationCredentialsFile { get;}
-        public TranslationClient Client { get;}
+        private readonly ITranslationProviderRepository _translationProviderRepository;
 
-        public GoogleTranslateProvider()
+        public string GoogleApplicationCredentialsFile { get; set; }
+        public string Name { get; }
+        public TranslationClient Client { get; set; }
+
+        public GoogleTranslateProvider(ITranslationProviderRepository translationProviderRepository)
         {
-            GoogleApplicationCredentialsFile = ConfigurationManager.AppSettings["GOOGLE_APPLICATION_CREDENTIALS_FILE"];
+            _translationProviderRepository = translationProviderRepository;
+
+            Name = "google";
+        }
+
+        public void CreateClient()
+        {
+            var provider = _translationProviderRepository.Select(x => x.Name == Name).Result;
+            GoogleApplicationCredentialsFile = provider.Value;
 
             GoogleCredential googleCredential;
-            using (var stream = new FileStream(GoogleApplicationCredentialsFile, FileMode.Open, FileAccess.Read))
-            {
-                googleCredential = GoogleCredential.FromStream(stream).CreateScoped();
-            }
+
+            googleCredential = GoogleCredential.FromJson(GoogleApplicationCredentialsFile).CreateScoped();
 
             Client = TranslationClient.Create(googleCredential);
         }
 
         public async Task<string> TranslateText(string textToTranslate, string targetLanguageIsoCode2, string sourceLanguageIsoCode2)
         {
+            if (Client == null)
+            {
+                CreateClient();
+            }
+
             var response = await Client.TranslateTextAsync(textToTranslate, targetLanguageIsoCode2, sourceLanguageIsoCode2);
 
             return response.TranslatedText;
