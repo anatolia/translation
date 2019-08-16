1f2094645d4833cec0a3da48a03ab2cc35be6916
CihatAskin <moralman26@gmail.com>
2019-08-16 18:31:57 +0300

    added TranslationProviders detail page

---
+++ Source/Translation.Client.Web/Models/TranslationProvider/TranslationProviderDetailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Translation.Client.Web.Models.Base;
+
+namespace Translation.Client.Web.Models.TranslationProvider
+{
+    public class TranslationProviderDetailModel : BaseModel
+    {
+        public Guid TranslationProviderUid { get; set; }
+        public string Value { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public TranslationProviderDetailModel()
+        {
+            Title = "translation_provider_detail_title";
+        }
+    }
+}
