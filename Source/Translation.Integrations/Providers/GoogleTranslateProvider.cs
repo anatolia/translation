ffce8a84f361ce3bf2ebde052f4ecaea01651935
CihatAskin <moralman26@gmail.com>
2019-08-20 16:01:40 +0300

    added related usage of API infos

--- Source/Translation.Integrations/Providers/GoogleTranslateProvider.cs
+++ Source/Translation.Integrations/Providers/GoogleTranslateProvider.cs
@@ -1,4 +1,22 @@
-﻿using System.Configuration;
+﻿/* Creating a GCP(Google Cloud Platform) Console project.
+ *********************************************************
+ 1.Sign in Google API Console from the link: https://console.cloud.google.com/apis/.
+ 2.Click on the “Select a project” on the Google Cloud Platform tab.
+ 3.Click on the “New Project”.
+ 4.Enter your project name and click on the “Create” button.
+ 5.Your project will be displayed on the Google Cloud Platform dashboard. Select “APIs and Services” on the navigation drawer at the left side.
+ 6.Click on the “Enable APIs and Services” button.
+ 7.Type “Cloud Translation API” in the search box and select the API
+ 8.You need to enable Cloud Translation API for your project by clicking on the “Enable” button You need to enable Cloud Translation API for your project by clicking on the “Enable” button
+ 9.You are ready to manage Cloud Translation API for your project now. Click on “Manage”
+ 10.Click on “Create Credentials”
+ 11.Select “Cloud Translation API”
+ 12.Enter your personal information, select “JSON” as the key type and click on “Continue”
+ 13.The credentials file of your project will be downloaded to your computer automatically in JSON format. Keep this file.
+ 14.You(Super Admin) will use this JSON file as TranslationProvider value editing Super admin dashboard translation_providers link
+  */
+
+using System.Configuration;
 using System.IO;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -14,11 +32,9 @@ namespace Translation.Integrations.Providers
     public class GoogleTranslateProvider : ITextTranslateProvider
     {
         private readonly ITranslationProviderRepository _translationProviderRepository;
-
         public string GoogleApplicationCredentialsFile { get; set; }
-        public string Name { get; }
         public TranslationClient Client { get; set; }
-
+        public string Name { get; set; }
         public GoogleTranslateProvider(ITranslationProviderRepository translationProviderRepository)
         {
             _translationProviderRepository = translationProviderRepository;
--- Source/Translation.Integrations/Providers/YandexTranslateProvider.cs
+++ Source/Translation.Integrations/Providers/YandexTranslateProvider.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections.Generic;
+﻿/* Usage of Yandex API.
+ *********************************************************
+ 1.Create an account from the link:https://tech.yandex.com/translate/.
+ 2.Click on the "documentation"
+ 3.you will find usage of parameters and API requirements under the "Translate Text" page
+ 4.Get a free API key and Keep this file.
+ 5.You(Super Admin) will use this API key as TranslationProvider value editing Super admin dashboard translation_providers link
+  */
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,10 +21,9 @@ namespace Translation.Integrations.Providers
     public class YandexTranslateProvider : ITextTranslateProvider
     {
         private readonly ITranslationProviderRepository _translationProviderRepository;
-        public string Name { get; }
         public string YandexTranslationApiKey { get; set; }
         private string RequestUrl { get; set; }
-
+        public string Name { get; set; }
         public YandexTranslateProvider(ITranslationProviderRepository translationProviderRepository)
         {
             _translationProviderRepository = translationProviderRepository;
@@ -30,9 +37,9 @@ namespace Translation.Integrations.Providers
             RequestUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate";
         }
 
-        public async Task<string> TranslateText(string textToTranslate, string sourceLanguageIsoCode2, string targetLanguageIsoCode2)
+        public async Task<string> TranslateText(string textToTranslate, string targetLanguageIsoCode2, string sourceLanguageIsoCode2)
         {
-            if (YandexTranslationApiKey==null || RequestUrl==null)
+            if (YandexTranslationApiKey == null || RequestUrl == null)
             {
                 CreateClient();
             }
