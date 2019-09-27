dda874c2f5dba5c07d6b21a927ea2d67906e977e
hasancabuk <hasan_cabuk_@hotmail.com>
2019-09-27 10:11:54 +0300

    Deleted gaps

--- Source/Translation.Common/Contracts/IOrganizationService.cs
+++ Source/Translation.Common/Contracts/IOrganizationService.cs
@@ -13,11 +13,9 @@ namespace Translation.Common.Contracts
     public interface IOrganizationService
     {
         Task<SignUpResponse> CreateOrganizationWithAdmin(SignUpRequest request);
-
         OrganizationReadResponse GetOrganization(OrganizationReadRequest request);
         Task<OrganizationReadListResponse> GetOrganizations(OrganizationReadListRequest request);
-        Task<OrganizationRevisionReadListResponse>
-            GetOrganizationRevisions(OrganizationRevisionReadListRequest request);
+        Task<OrganizationRevisionReadListResponse> GetOrganizationRevisions(OrganizationRevisionReadListRequest request);
         Task<OrganizationEditResponse> EditOrganization(OrganizationEditRequest request);
         Task<OrganizationRestoreResponse> RestoreOrganization(OrganizationRestoreRequest request);
         Task<OrganizationPendingTranslationReadListResponse> GetPendingTranslations(OrganizationPendingTranslationReadListRequest request);
@@ -27,23 +25,18 @@ namespace Translation.Common.Contracts
         Task<PasswordResetValidateResponse> ValidatePasswordReset(PasswordResetValidateRequest request);
         Task<PasswordResetResponse> PasswordReset(PasswordResetRequest request);
         Task<PasswordChangeResponse> ChangePassword(PasswordChangeRequest request);
-
         Task<UserChangeActivationResponse> ChangeActivationForUser(UserChangeActivationRequest request);
         Task<UserEditResponse> EditUser(UserEditRequest request);
         Task<UserDeleteResponse> DeleteUser(UserDeleteRequest request);
-        
         Task<UserInviteResponse> InviteUser(UserInviteRequest request);
         Task<UserInviteValidateResponse> ValidateUserInvitation(UserInviteValidateRequest request);
         Task<UserAcceptInviteResponse> AcceptInvitation(UserAcceptInviteRequest request);
-
         UserReadResponse GetUser(UserReadRequest request);
         Task<UserReadListResponse> GetUsers(UserReadListRequest request);
         Task<UserRevisionReadListResponse> GetUserRevisions(UserRevisionReadListRequest request);
         Task<UserLoginLogReadListResponse> GetUserLoginLogs(UserLoginLogReadListRequest request);
         Task<OrganizationLoginLogReadListResponse> GetUserLoginLogsOfOrganization(OrganizationLoginLogReadListRequest request);
-        
         CurrentUser GetCurrentUser(CurrentUserRequest request);
-
         Task<bool> LoadOrganizationsToCache();
         Task<bool> LoadUsersToCache();
         Task<UserRestoreResponse> RestoreUser(UserRestoreRequest request);
