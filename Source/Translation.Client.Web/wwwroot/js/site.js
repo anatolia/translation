c6e5231f9e05f1b34587ce1cf3f2ffef975dac4b
Hasan <hasan_cabuk_@hotmail.com>
2019-11-15 18:02:12 +0300

    Added translation js

--- Source/Translation.Client.Web/wwwroot/js/site.js
+++ Source/Translation.Client.Web/wwwroot/js/site.js
@@ -48,7 +48,6 @@ function getParent(element, parentClass) {
     });
 }
 
-
 function getElement(id) { return document.getElementById(id); }
 function getValue(id) { return document.getElementById(id).value; }
 function getAntiForgeryTokenFromLayout() { return document.getElementsByName('__RequestVerificationToken')[0].value; }
@@ -146,95 +145,6 @@ function doRedirectIfConfirmedSuccess(btn, redirectUrl) {
     });
 }
 
-function translateScreen() {
-    translateElement(document.head);
-    translateElement(document.body);
-}
-
-function translateElement(element) {
-    if (element === null
-        || element === undefined) {
-        return;
-    }
-
-    if (labels === null
-        || labels === undefined) {
-        return;
-    }
-  
-    let defaultLang = 'en';
-    if (currentUser !== undefined
-        && currentUser !== null) {
-        defaultLang = currentUser.languageIsoCode2Char;
-    }
-
-    let placeholders = element.querySelectorAll('[placeholder]');
-    placeholders.forEach(function (item) {
-        for (let i = 0; i < labels.length; i++) {
-            let label = labels[i];
-            if (label.key === item.placeholder) {
-                label.translations.forEach(function (translation) {
-                    if (translation.languageIsoCode2 === defaultLang) {
-                        item.setAttribute('placeholder', translation.translation);
-                        return;
-                    }
-                });
-
-                break;
-            }
-        }
-    });
-
-    let items = element.querySelectorAll('[data-translation]');
-    items.forEach(function (item) {
-        for (let i = 0; i < labels.length; i++) {
-            let label = labels[i];
-            if (label.key === item.dataset.translation) {
-                label.translations.forEach(function (translation) {
-                    if (translation.languageIsoCode2 === defaultLang) {
-                        item.innerHTML = translation.translation;
-                        return;
-                    }
-                });
-
-                break;
-            }
-        }
-    });
-}
-
-let currentUser = null;
-doGet('/Data/GetCurrentUser', function (req) {
-    if (199 < req.status && req.status < 300) {
-        if (req.status === 200
-            && req.responseText !== null) {
-            localStorage.setItem('currentUser', req.responseText);
-            currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        } else if (req.status === 200
-            && req.responseText === null) {
-            currentUser = null;
-            window.redirect('/Login');
-        }
-        else if (req.status === 500) {
-            localStorage.clear();
-            window.redirect('/Login');
-        }
-    }
-});
-
-let labels = JSON.parse(localStorage.getItem('labels'));
-if (labels == null
-    || labels.length === 0) {
-    doGet('/Data/GetMainLabels', function (req) {
-        if (199 < req.status && req.status < 300) {
-            labels = localStorage.setItem('labels', req.responseText);
-            translateScreen();
-        }
-    });
-} else {
-    translateScreen();
-}
-
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tab-content");
@@ -248,6 +158,7 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
 function SecretField(element, order) {
     this.el = element;
     this.order = order;
