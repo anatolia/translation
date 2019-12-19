3defed6fb799987084ca6470cea7e405fcda40a5
Hasan <hasan_cabuk_@hotmail.com>
2019-12-19 17:45:53 +0300

    Refactored requests

--- Source/Translation.Common/Models/Requests/Organization/OrganizationPendingTranslationReadListRequest.cs
+++ Source/Translation.Common/Models/Requests/Organization/OrganizationPendingTranslationReadListRequest.cs
@@ -1,22 +1,12 @@
-﻿using System;
-
-using StandardUtils.Helpers;
-using StandardUtils.Models.Requests;
+﻿using StandardUtils.Models.Requests;
 
 namespace Translation.Common.Models.Requests.Organization
 {
     public sealed class OrganizationPendingTranslationReadListRequest : BaseAuthenticatedPagedRequest
     {
-        public Guid OrganizationUid { get; }
-
-        public OrganizationPendingTranslationReadListRequest(long currentUserId, Guid organizationUid) : base(currentUserId)
+        public OrganizationPendingTranslationReadListRequest(long currentUserId) : base(currentUserId)
         {
-            if (organizationUid.IsEmptyGuid())
-            {
-                ThrowArgumentException(nameof(organizationUid), organizationUid);
-            }
 
-            OrganizationUid = organizationUid;
         }
     }
 }
--- Source/Translation.Common/Models/Requests/Project/ProjectReadListRequest.cs
+++ Source/Translation.Common/Models/Requests/Project/ProjectReadListRequest.cs
@@ -1,22 +1,11 @@
-﻿using System;
-
-using StandardUtils.Helpers;
-using StandardUtils.Models.Requests;
+﻿using StandardUtils.Models.Requests;
 
 namespace Translation.Common.Models.Requests.Project
 {
     public sealed class ProjectReadListRequest : BaseAuthenticatedPagedRequest
     {
-        public Guid OrganizationUid { get; }
-
-        public ProjectReadListRequest(long currentUserId, Guid organizationUid) : base(currentUserId)
+        public ProjectReadListRequest(long currentUserId) : base(currentUserId)
         {
-            if (organizationUid.IsEmptyGuid())
-            {
-                ThrowArgumentException(nameof(organizationUid), organizationUid);
-            }
-            
-            OrganizationUid = organizationUid;
         }
     }
 }
